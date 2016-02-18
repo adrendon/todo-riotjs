ee3607af16d76153761070b92b8144d994b9bca7
alexis rendón <alexis9229@hotmail.com>
2016-02-18 09:17:42 -0500

    todo-list

---
+++ README.txt
@@ -0,0 +1,7 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/adrendon9229/pen/KVLKdJ.
+
+ Just a simple Todo app with Riot.js
+
+Forked from [Adam Lindqvist](http://codepen.io/adamlindqvist/)'s Pen [Todo](http://codepen.io/adamlindqvist/pen/EaQNGP/).
+
+Forked from [Adam Lindqvist](http://codepen.io/adamlindqvist/)'s Pen [Todo](http://codepen.io/adamlindqvist/pen/EaQNGP/).
---
+++ js/index.js
@@ -0,0 +1,55 @@
+
+riot.tag('todo', '<div class="add" onclick="{ showAdd }"><i class="fa fa-plus"></i></div> <div class="title"><h1>{ opts.title }</h1></div> <div class="new-task"><a href="#" class="add-new" onclick="{ add }"><i class="fa fa-plus"></i></a> <input name="input" placeholder="New task"  onkeyup="{ edit }"/> </div> <ul> <li class="row { done: done }" each="{ items.filter(filter) }"> <a class="remove" onclick="{ parent.removeItem }"><i class="fa fa-trash-o"></i></a><a class="completed" onclick="{ parent.toggle }"><i class="fa fa-check"></i></a><span>{ title }</span> </li> </ul>', function(opts) {
+  this.items = opts.items
+    
+  this.edit = function(e) {
+    this.text = e.target.value
+    if (e.which == 13)
+      this.add()
+  }.bind(this)
+
+  this.add = function(e) {
+    if (this.text) {
+      this.items.push({ title: this.text, done: false })
+      this.text = this.input.value = ''
+      this.update()
+    }
+  }.bind(this)
+
+  this.filter = function(item) {
+    return !item.hidden
+  }.bind(this)
+
+  this.toggle = function(e) {
+    var item = e.item
+    item.done = !item.done
+    return true
+  }.bind(this)
+  
+  this.removeItem = function(e) {
+    var item = e.item
+    item.hidden = true
+    this.update()
+    return true
+  }.bind(this)  
+
+  this.showAdd = function(e) {
+    $(".new-task").slideToggle();
+  	  $("#todo-text").focus();
+    return true
+  }.bind(this)
+  
+})
+
+riot.mount('todo', {
+  title: "Things to do",
+  items: [
+    { title: 'Go to the bank', done: false, hidden: false },
+    { title: 'Buy food', done: true },
+    { title: 'Clean the house', done: false },
+    { title: 'Make dinner', done: false },
+    { title: 'Pick up the kids', done: false }
+  ]
+});
+
+$( "ul" ).sortable();
