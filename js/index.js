44856a7711b6899efb9da66b69bcec8f83866517
alexis rendón <alexis9229@hotmail.com>
2016-05-06 11:30:17 -0500

    Todo list

--- js/index.js
+++ js/index.js
@@ -42,28 +42,13 @@ riot.tag('todo', '<div class="add" onclick="{ showAdd }"><i class="fa fa-plus"><
 })
 
 riot.mount('todo', {
-  title: "Tareas - Interact",
+  title: "Tareas",
   items: [
-    { title: 'Creación Html - Ofertas', done: false },
-    { title: 'Html, Imagenes en Portal', done: false },
-    { title: 'Tokens Imagenes en Portal', done: false },
-    { title: 'Tokens Ofertas Portal', done: false },
-    { title: 'Punto de Interacción en Portal', done: false },
-    { title: 'Creación Ofertas Default Portal', done: false },
-    { title: 'Creación Zonas en Interact', done: false },
-    { title: 'Creación de Puntos de Interacción en Portal', done: false },
-    { title: 'Creación de Flowchart', done: false },
-    { title: 'Creación de Interaction, Decisión (dbo.customer=valor)', done: false },
-    { title: 'Creación de PopulateSeg - Crear segmentos', done: false },
-    { title: 'Crear Ofertas - Interact', done: false },
-    { title: 'Crear Campaña - Interact', done: false },
-    { title: 'Crear Estrategia (Segmentos, Zonas, Ofertas)', done: false },
-    { title: 'Implementar Scripts', done: false },
-    { title: 'Cambiar Scripts - Dllo-Pdn', done: false },
-    { title: 'Desplegar Dllo', done: false },
-    { title: 'Probar Ofertas', done: false },
-    { title: 'Probar Analitycs', done: false },
-    { title: 'Desplegar Pdn', done: false }
+    { title: 'Aprender Js', done: false, hidden: false },
+    { title: 'Aprender Meteor', done: true },
+    { title: 'Aprender js', done: false },
+    { title: 'Aprender Css3', done: false },
+    { title: 'Aprender php', done: false }
   ]
 });
 
